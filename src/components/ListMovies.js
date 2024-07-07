bbbdd27cf567b63be0f69117a693b5dd084fd642
Benjamín Isasmendi C <isasmendi223@gmail.com>
2024-07-06 20:33:39 -0400

    a

---
+++ src/components/ListMovies.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ListMovies = (props) => {
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            </div>
+        </div>
+    </div>
+}
